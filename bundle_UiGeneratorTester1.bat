f52b0386a7d7681a2d204fcacf7b46bec34f4653
Claudius Iacob <claudius.iacob@gmail.com>
2019-04-01 12:13:07 +0300

    -- adding custom bundle scripts

---
+++ bundle_UiGeneratorTester1.bat
@@ -0,0 +1,18 @@
+echo off
+
+SET javaDir=k:\DATA\RW\PROJECTS\_PROGRAMMING_\_PROJECTS_\_BUILD_\java\bin
+SET sdkDir=k:\DATA\RW\PROJECTS\_PROGRAMMING_\_PROJECTS_\_BUILD_\sdk\win64--Flex-4-16-1--AIR-28
+SET sdkBinDir=%sdkDir%\bin
+SET sdkJarsDir=%sdkDir%\lib
+SET frameworksDir=%sdkDir%\frameworks
+SET certHome=k:\DATA\RW\Keys
+
+REM could not get relative paths to properly work, so we are going to invoke ADT from INSIDE the release folder...
+cd bin-debug
+
+%javaDir%\java -jar %sdkJarsDir%\adt.jar -package -storetype pkcs12 -keystore %certHome%\claudius-iacob-certificate -target bundle ../bin-release/UiGeneratorTester1 UiGeneratorTester1-app.xml UiGeneratorTester1.swf
+
+REM ...and then move back
+cd ../
+
+pause
